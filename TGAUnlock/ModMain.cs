77e1c7782212f42fff620cab4a12653264925814
sbcomputertech <sbcomputertech.0@gmail.com>
2023-04-26 22:17:30 +0100

    init commit - works

---
+++ TGAUnlock/ModMain.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace UnlockMod;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class ModMain : BaseUnityPlugin
+{
+	private void Awake()
+	{
+		Logger.LogInfo($"{PluginInfo.PLUGIN_NAME} ver {PluginInfo.PLUGIN_VERSION} loaded!"); // print a message to the BepInEx console
+		var harmony = new Harmony(PluginInfo.PLUGIN_GUID); // create a harmony patcher
+		harmony.PatchAll(); // run all patches in the mod assembly
+	}
+}
+
+[HarmonyPatch(typeof(Player_DevConsole), nameof(Player_DevConsole.Update))]
+public static class DevConsolePatch
+{
+	public static void Prefix(ref Player_DevConsole __instance)
+	{
+		__instance.NEVER = false;
+		__instance.panelConsole.transform.position = new Vector3(500, 400, 0);
+	}
+}
---
+++ TGAUnlock/PluginInfo.cs
@@ -0,0 +1,8 @@
+namespace UnlockMod;
+
+public static class PluginInfo
+{
+	public const string PLUGIN_NAME = "TGA Unlocker";
+	public const string PLUGIN_GUID = "com.calliefox.tga.unlocker";
+	public const string PLUGIN_VERSION = "1.0.0";
+}
